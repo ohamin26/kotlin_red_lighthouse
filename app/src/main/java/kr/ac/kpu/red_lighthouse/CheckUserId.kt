fe1202ef70bffde1d684adaf1e5492032b96f190
ohamin <ohamin26@gmail.com>
2023-05-10 00:15:46 +0900

    로그인 기능 추가

---
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/CheckUserId.kt
@@ -0,0 +1,18 @@
+package kr.ac.kpu.red_lighthouse
+
+import java.util.regex.Pattern
+
+class CheckUserId {
+    //아이디 형식 검사
+    fun checkEmail(email : String): Boolean {
+        val emailValidation = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        val check = email.trim()
+        return Pattern.matches(emailValidation, check)
+    }
+    //비밀번호 형식 검사
+    fun checkPw(pw : String): Boolean {
+        val pwValidation = "^.*(?=^.{8,15}\$)(?=.*\\d)(?=.*[a-zA-Z])(?=.*[!@#\$%^&+=]).*\$"
+        val check = pw.trim()
+        return Pattern.matches(pwValidation, check)
+    }
+}
--- app/src/main/java/kr/ac/kpu/red_lighthouse/LoginActivity.kt
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/LoginActivity.kt
@@ -16,36 +16,39 @@ class LoginActivity : Activity(){
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val id : EditText = binding.loginId
-        val pw : EditText = binding.loginPw
-        val loginFind : Button = binding.findLogin
-        val loginSubmit : Button = binding.login
-        fun checkEmail(): Boolean {
-            val emailValidation = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
-            val check = id.text.toString().trim()
-            return Pattern.matches(emailValidation, check)
-        }
-        loginSubmit.setOnClickListener {
-            if("".equals(id.text.toString())){
+        val userId : EditText = binding.userId
+        val userPw : EditText = binding.userPw
+        val userPwFindBtn : Button = binding.userPwFindBtn
+        val loginSubmitBtn : Button = binding.loginSubmitBtn
+        // 로그인 시도 이벤트
+        // 아이디나 비밀번호나 공백이거나 형식에 맞지 않을 시 다시 입력하게 한다.
+        // 맞을 경우 페이지 이동 로그인 정보 전송
+        loginSubmitBtn.setOnClickListener {
+            if("".equals(userId.text.toString())){
                 Toast.makeText(this@LoginActivity,"아이디를 입력하세요",Toast.LENGTH_SHORT).show()
-                id.requestFocus()
+                userId.requestFocus()
             }
-            else if(!checkEmail()){
+            else if(!CheckUserId().checkEmail(userId.text.toString())){
                 Toast.makeText(this@LoginActivity,"올바른 이메일이 아닙니다",Toast.LENGTH_SHORT).show()
-                id.requestFocus()
+                userId.requestFocus()
             }
-            else if("".equals(pw.text.toString())){
+            else if("".equals(userPw.text.toString())){
                 Toast.makeText(this@LoginActivity,"비밀번호를 입력하세요",Toast.LENGTH_SHORT).show()
-                pw.requestFocus()
+                userPw.requestFocus()
+            }
+            else if(!CheckUserId().checkPw(userPw.text.toString())){
+                Toast.makeText(this@LoginActivity,"비밀번호는 숫자,문자,특수문자 포함 8~15자 이내로 입력하세요",Toast.LENGTH_SHORT).show()
+                userPw.requestFocus()
             }
             else{
                 var intent = Intent(applicationContext, MapActivity::class.java)
-                intent.putExtra("id",id.text.toString())
-                intent.putExtra("pw",pw.text.toString())
+                intent.putExtra("id",userId.text.toString())
+                intent.putExtra("pw",userPw.text.toString())
                 startActivity(intent)
             }
         }
-        loginFind.setOnClickListener {
+        // 비밀번호 찾기 화면 이동 이벤트
+        userPwFindBtn.setOnClickListener {
             var intent = Intent(applicationContext, FindIdActivity::class.java)
             startActivityForResult(intent,0)
         }

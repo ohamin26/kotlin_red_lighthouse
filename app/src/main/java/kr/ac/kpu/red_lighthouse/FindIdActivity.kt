69753c99e81616b19db6e965a4ff4a35407dfea4
ohamin <ohamin26@gmail.com>
2023-05-10 00:14:16 +0900

    비밀번호 찾기 기능 추가

--- app/src/main/java/kr/ac/kpu/red_lighthouse/FindIdActivity.kt
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/FindIdActivity.kt
@@ -1,12 +1,11 @@
 package kr.ac.kpu.red_lighthouse
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import kr.ac.kpu.red_lighthouse.databinding.ActivityFindidBinding
-import kr.ac.kpu.red_lighthouse.databinding.ActivityLoginBinding
-import java.util.regex.Pattern
 
 class FindIdActivity : Activity(){
 
@@ -16,19 +15,23 @@ class FindIdActivity : Activity(){
         super.onCreate(savedInstanceState)
         binding = ActivityFindidBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        lateinit var pw : String
-        val dataId = "abc@naver.com"
-        val inputId = binding.findId
+        lateinit var dataPw : String // 임시 데이터 -비밀번호 저장용
+        val dataId = "abc@naver.com" // 임시 데이터 -아이디 저장용
+        val inputId = binding.userId
         val checkIdBtn = binding.checkIdBtn
         val newPwBtn = binding.newPwBtn
-        var findPw = binding.findPw
-        fun checkEmail(): Boolean {
-            val emailValidation = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
-            val check = inputId.text.toString().trim()
-            return Pattern.matches(emailValidation, check)
+        val newPw = binding.findPw
+        run {
+            binding.goLogin.setOnClickListener {
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            }
         }
+        // 아이디 버튼 이벤트
+        // 아이디 형식과 다르거나 데이터에 없는 아이디일 경우 경우 다시 입력하게 한다
+        // 맞을 경우 새로운 비밀번호 생성 화면 표시
         checkIdBtn.setOnClickListener {
-            if(!checkEmail() || dataId.equals(inputId.text.toString())){
+            if(CheckUserId().checkEmail(inputId.text.toString()) || !(inputId.text.toString().equals(dataId))){
                 Toast.makeText(this@FindIdActivity,"올바른 이메일이 아닙니다", Toast.LENGTH_SHORT).show()
                 inputId.requestFocus()
             }else{
@@ -36,10 +39,19 @@ class FindIdActivity : Activity(){
                 binding.pwWindow.visibility = View.VISIBLE
             }
         }
+        // 비밀번호 버튼 이벤트
+        // 비밀번호 형식에 맞지 않거나 공백일 시 다시 입력하게 한다.
+        // 맞을 경우 토스트 메시지 출력 및 로그인 화면으로 이동
         newPwBtn.setOnClickListener {
-            pw = findPw.text.toString()
-            Toast.makeText(this@FindIdActivity,"비밀번호가 변경되었습니다.",Toast.LENGTH_SHORT).show()
-            newPwBtn.visibility = View.GONE
+            if(CheckUserId().checkPw(newPw.text.toString()) ||"".equals(newPw.text.toString())){
+                Toast.makeText(this@FindIdActivity,"비밀번호는 숫자,문자,특수문자 포함 8~15자 이내로 입력하세요",Toast.LENGTH_SHORT).show()
+                newPw.requestFocus()
+            }else{
+                dataPw = newPw.text.toString()
+                Toast.makeText(this@FindIdActivity,"비밀번호가 변경되었습니다.",Toast.LENGTH_SHORT).show()
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }

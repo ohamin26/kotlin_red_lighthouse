d6fa6cfdfb6782ad7bebf1dc898f1b1f35403fee
ohamin <ohamin26@gmail.com>
2023-05-11 16:57:16 +0900

    자동 로그인 기능 추가

--- app/src/main/java/kr/ac/kpu/red_lighthouse/RegisterActivity.kt
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/RegisterActivity.kt
@@ -1,14 +1,10 @@
 package kr.ac.kpu.red_lighthouse
 
-
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity

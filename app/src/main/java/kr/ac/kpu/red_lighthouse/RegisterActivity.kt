856ea55912ccbc321fd7175b0ce47139976a0c1e
quokka12 <znjzk0636@naver.com>
2023-05-12 16:17:19 +0900

    진형 : 데이터베이스 회원가입 기능, 로그인 기능 구현(setSharedPreference,FirebaseAuth,FireStore)

--- app/src/main/java/kr/ac/kpu/red_lighthouse/RegisterActivity.kt
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/RegisterActivity.kt
@@ -12,15 +12,17 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.PrecomputedTextCompat
+import androidx.core.widget.TextViewCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObjects
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
 import kr.ac.kpu.red_lighthouse.databinding.ActivityRegisterBinding
+import kr.ac.kpu.red_lighthouse.user.User
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.regex.Pattern
@@ -33,20 +35,21 @@ class RegisterActivity : AppCompatActivity() {
     var isExistBlank = false
     var isPWSame = false
 
-    private var auth : FirebaseAuth = Firebase.auth
+    private lateinit var auth: FirebaseAuth
     private val db = Firebase.firestore
-    lateinit var userInfo : HashMap<String,String?>
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
-        val btn_register : Button = binding.btnRegister
-        val edit_pw : EditText = binding.editPw
-        val edit_id : EditText = binding.editId
-        val edit_pw_re : EditText = binding.editPwRe
+
+        val btn_register: Button = binding.btnRegister
+        val edit_pw: EditText = binding.editPw
+        val edit_id: EditText = binding.editId
+        val edit_pw_re: EditText = binding.editPwRe
         val edit_name: EditText = binding.editName
 
         btn_register.setOnClickListener {
@@ -58,41 +61,59 @@ class RegisterActivity : AppCompatActivity() {
             val nickname = binding.editName.text.toString()
 
             // 유저가 항목을 다 채우지 않았을 경우
-            if(email.isEmpty() || password.isEmpty() || pw_re.isEmpty() || nickname.isEmpty()){
+            if (email.isEmpty() || password.isEmpty() || pw_re.isEmpty() || nickname.isEmpty()) {
                 isExistBlank = true
-            }
-            else{
+            } else {
 
-                if(password == pw_re){
+                if (password == pw_re) {
                     isPWSame = true
                 }
             }
 
-            if(!isExistBlank && isPWSame){
-                userInfo = hashMapOf(
-                    "uid" to "",
-                    "email" to email,
-                    "password" to password,
-                    "nickname" to nickname,
-                    "dateOfRegist" to ""
-                )
-                CoroutineScope(Dispatchers.Main).launch {
-                    createAccount()
-                }
-         } else{
+            if (!isExistBlank && isPWSame) {
+                try {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        val ref = db.collection("users")
+                        val onlyDate: LocalDate = LocalDate.now()
+                        var checkUser : List<User> = ref.whereEqualTo("user_email",email).get().await().toObjects(User::class.java)
+                        if(checkUser.isNotEmpty()) {//데이터베이스 안에 같은 이메일이 존재 한다면 알려주는 메세지.
+                            Toast.makeText(applicationContext, "이미 가입된 계정입니다.", Toast.LENGTH_SHORT).show()
+                        }else{
+                            var user  = User("",email,password,nickname,onlyDate.toString())
+                            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener()
+                            { task ->
+                                if (task.isSuccessful) {
+                                    val uid : String? = auth.currentUser?.uid
+                                    if(uid !=null){ //UserId가 null이 아닐 때 데이터베이스 정보 저장 명령 실행
+                                        user.user_id = uid;
+                                        ref.document(uid).set(user)
+                                        finish() // 가입창 종료
+                                        Toast.makeText(applicationContext, "회원가입 성공. 로그인해주세요.", Toast.LENGTH_SHORT).show()
+                                    }
+                                }else{
+                                    //회원가입 실패 메세지 출력
+                                    Toast.makeText(applicationContext, "회원가입에 실패했습니다. 나중에 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        }
 
-               // 상태에 따라 다른 다이얼로그 띄워주기
-               if(isExistBlank){   // 작성 안한 항목이 있을 경우
+                    }
+                }catch (e : Error){
+                    //에러 메세지 출력
+                    print(e)
+                    Toast.makeText(applicationContext, "회원가입에 실패했습니다. 나중에 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                // 상태에 따라 다른 다이얼로그 띄워주기
+                if (isExistBlank) {   // 작성 안한 항목이 있을 경우
                     dialog("blank")
-              }
-               else if(!isPWSame){ // 입력한 비밀번호가 다를 경우
+                } else if (!isPWSame) { // 입력한 비밀번호가 다를 경우
                     dialog("not same")
-               }
+                }
             }
 
         }
     }
-
     // 회원가입 실패시 다이얼로그를 띄워주는 메소드
     fun dialog(type: String){
         val dialog = AlertDialog.Builder(this)
@@ -128,32 +149,5 @@ class RegisterActivity : AppCompatActivity() {
 
         dialog.setPositiveButton("확인",dialog_listener)
         dialog.show()
-    }
-    // 계정 생성
-    @RequiresApi(Build.VERSION_CODES.O)
-    suspend fun createAccount() {
-        if (userInfo["email"] != "" && userInfo["password"] != "" && userInfo["nickname"] != "") { //입련한 정보가 null이 아닐 때 실행.
-            auth?.createUserWithEmailAndPassword(userInfo["email"].toString(), userInfo["password"].toString()) //계정 생성
-                ?.addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        val onlyDate: LocalDate = LocalDate.now()
-                        val uid : String? = auth.currentUser?.uid
-                        userInfo["uid"] = uid
-                        userInfo["dateOfRegist"]=onlyDate.toString()
-                        if(uid !=null){ //UserId가 null이 아닐 때 데이터베이스 정보 저장 명령 실행
-                            db.collection("users").document() //데이터베이스 정보 저장
-                                .set(userInfo)
-                                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
-                                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
-                        }
-                        finish() // 가입창 종료
-                    } else {
-                        Toast.makeText(
-                            this, "계정 생성 실패",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-        }
     }
 }
---
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/user/User.kt
@@ -0,0 +1,22 @@
+package kr.ac.kpu.red_lighthouse.user
+
+class User () {
+    var user_id : String? = ""
+    var user_email : String? = ""
+    var user_password : String? = ""
+    var user_nickname : String? = ""
+    var user_dateOfRegist : String? = ""
+
+    constructor(user_id:String,user_email:String,user_password:String,user_nickname:String,user_dateOfRegist:String) : this() {
+        this.user_id = user_id
+        this.user_email = user_email
+        this.user_password = user_password
+        this.user_nickname = user_nickname
+        this.user_dateOfRegist = user_dateOfRegist
+    }
+
+    fun setSharedPreference(){
+
+    }
+
+}

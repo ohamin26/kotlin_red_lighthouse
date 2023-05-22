f997230c12695b9ad64a204899d08d1c1b273985
quokka12 <znjzk0636@naver.com>
2023-05-23 01:06:41 +0900

    명소 추가 화면 구현

---
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/activity/LocationAddActivity.kt
@@ -0,0 +1,12 @@
+package kr.ac.kpu.red_lighthouse.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kpu.red_lighthouse.R
+
+class LocationAddActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location_add)
+    }
+}

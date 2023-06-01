5637edf9916585a06b2bd7f91829b5671a75c6be
SoominKo <80901129+Gosuke716@users.noreply.github.com>
2023-06-01 12:00:20 +0900

    구글맵 지도 연결/ 내위치 / 검색은 추후 추가

--- app/src/main/java/kr/ac/kpu/red_lighthouse/activity/MapActivity.kt
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/activity/MapActivity.kt
@@ -1,24 +1,155 @@
 package kr.ac.kpu.red_lighthouse.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.location.LocationRequest
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationListener
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kr.ac.kpu.red_lighthouse.R
-import kr.ac.kpu.red_lighthouse.databinding.ActivityMapBinding
 
-class MapActivity : Fragment() {
+class MapActivity : Fragment(), OnMapReadyCallback {
+    private lateinit var mView: MapView
+    var mLocationManager: LocationManager? = null
+    var mLocationListener: LocationListener? = null
+    lateinit var button: Button
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
+    private lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
+    private lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장하는
+    private val REQUEST_PERMISSION_LOCATION = 10
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = ActivityMapBinding.inflate(inflater, container,false)
+        var rootView = inflater.inflate(R.layout.activity_map, container, false)
+        mView = rootView.findViewById(R.id.mapView)
+        button = rootView.findViewById(R.id.mapBtn)
+        mView.onCreate(savedInstanceState)
+        mView.getMapAsync(this)
+        button.setOnClickListener{
+            if(checkPermissionForLocation(requireContext())){
+                startLocationUpdates()
+            }
+        }
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+//        mFusedLocationProviderClient?.requestLocationUpdates(
+//            mLocationRequest,
+//            Looper.getMainLooper()
+//        )
+    }
+
+    private fun checkPermissionForLocation(context: android.content.Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                true
+            } else {
+                requestPermissions(
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSION_LOCATION
+                )
+                false
+            }
+        } else {
+            true
+        }
+    }
+
+    //내위치 추적 권한 허용코드
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+            } else {
+                Log.d("ttt", "onRequestPermissionsResult() _ 권한 허용 거부")
+                Toast.makeText(
+                    requireContext(),
+                    "권한이 없어 해당 기능을 실행할 수 없습니다.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    //최초로는 오이도 빨간등대가 나오도록
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        val marker = LatLng(37.3455424,126.6879337)
+        googleMap.addMarker(MarkerOptions().position(marker).title("빨간등대"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+    }
 
 
-        var text = binding.text
-        text.text = "Map Activity"
-        return binding.root
+    override fun onStart() {
+        super.onStart()
+        mView.onStart()
+    }
+    override fun onStop() {
+        super.onStop()
+        mView.onStop()
+    }
+    override fun onResume() {
+        super.onResume()
+        mView.onResume()
+    }
+    override fun onPause() {
+        super.onPause()
+        mView.onPause()
+    }
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mView.onLowMemory()
+    }
+    override fun onDestroy() {
+        mView.onDestroy()
+        super.onDestroy()
     }
 }
+
+

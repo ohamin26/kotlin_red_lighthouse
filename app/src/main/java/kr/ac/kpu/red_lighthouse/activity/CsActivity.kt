a9865fbb26b8bedf053a8488285c6272c5b2bd8d
sejinzx <132969962+sejinzx@users.noreply.github.com>
2023-06-06 17:08:12 +0900

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 1cf1e91f4729122a63d533b604d997c0a6cd6f0f, reversing
    changes made to 06a38812fd094675c49fceff7e3e9bd34f51f31e.

--- app/src/main/java/kr/ac/kpu/red_lighthouse/activity/CsActivity.kt
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/activity/CsActivity.kt
@@ -3,20 +3,11 @@ package kr.ac.kpu.red_lighthouse.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kr.ac.kpu.red_lighthouse.R
-import kr.ac.kpu.red_lighthouse.databinding.ActivityChangeInfoBinding
-import kr.ac.kpu.red_lighthouse.databinding.ActivityCsBinding
 
 class CsActivity : AppCompatActivity(){
-
-    private lateinit var binding: ActivityCsBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityCsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.btnBack.setOnClickListener {
-            finish()
-        }
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cs)
     }
 }

adc135f7f4a9b615a8c1d1b48c865b690487def6
ohamin26 <ohamin26@gmail.com>
2023-06-20 12:10:51 +0900

    수정

--- app/src/main/java/kr/ac/kpu/red_lighthouse/activity/ChangeInfoNicknameActivity.kt
+++ app/src/main/java/kr/ac/kpu/red_lighthouse/activity/ChangeInfoNicknameActivity.kt
@@ -12,9 +12,7 @@ import kr.ac.kpu.red_lighthouse.user.UserDao
 
 class ChangeInfoNicknameActivity : AppCompatActivity() {
     private  lateinit var binding: ActivityChangeInfoNicknameBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         binding = ActivityChangeInfoNicknameBinding.inflate(layoutInflater)
         var userDao = UserDao()
@@ -23,7 +21,10 @@ class ChangeInfoNicknameActivity : AppCompatActivity() {
         val userId = prefs.getString("userId"," ").toString()
         setContentView(binding.root)
         val btnChange = binding.btnChange
-        
+        //뒤로가기
+        binding.btnBack.setOnClickListener{
+            finish()
+        }
         //닉네임 변경 이벤트
         btnChange.setOnClickListener {
             // 공백 체크
@@ -31,15 +32,14 @@ class ChangeInfoNicknameActivity : AppCompatActivity() {
             if("" == nicknameChange){
                 Toast.makeText(this@ChangeInfoNicknameActivity,"닉네임을 입력하세요", Toast.LENGTH_SHORT).show()
                 binding.nicknameChange.requestFocus()
-
-            // 공백이 아닐 시 닉네임 변경, 화면 이동
+                // 공백이 아닐 시 닉네임 변경, 화면 이동
             }else{
                 userDao.updateNickname(userId, nicknameChange)
                 edit.putString("userNickname",nicknameChange)
                 edit.apply()
-                var intent = Intent(applicationContext, MenuSelectActivity::class.java)
-                intent.putExtra("user_info_changed",0)
+                var intent = Intent(applicationContext, ChangeInfoActivity::class.java)
                 startActivity(intent)
+                finish()
             }
         }
     }
